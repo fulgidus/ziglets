412cd819f8ef8c1a9975c055455915b838ad4697
QCSNET Alessio Corsi <alessio.corsi@qcsnet.it>
2025-05-21 14:09:35 +0200

    Traduci i messaggi di output in inglese per coerenza

--- src/guess.zig
+++ src/guess.zig
@@ -4,22 +4,22 @@ pub fn run() !void {
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
     const secret = std.crypto.random.intRangeAtMost(u32, 1, 100);
-    try stdout.print("Indovina il numero (1-100):\n", .{});
+    try stdout.print("Guess the number (1-100):\n", .{});
     while (true) {
         try stdout.print("> ", .{});
         var buf: [16]u8 = undefined;
         const line = try stdin.readUntilDelimiterOrEof(&buf, '\n');
         if (line == null) break;
         const guess = std.fmt.parseInt(u32, line.?, 10) catch {
-            try stdout.print("Per favore inserisci un numero valido.\n", .{});
+            try stdout.print("Please enter a valid number.\n", .{});
             continue;
         };
         if (guess < secret) {
-            try stdout.print("Troppo basso!\n", .{});
+            try stdout.print("Too low!\n", .{});
         } else if (guess > secret) {
-            try stdout.print("Troppo alto!\n", .{});
+            try stdout.print("Too high!\n", .{});
         } else {
-            try stdout.print("Bravo! Il numero era {d}.\n", .{secret});
+            try stdout.print("Nice! The number was {d}.\n", .{secret});
             break;
         }
     }
